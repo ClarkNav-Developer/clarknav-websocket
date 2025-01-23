74ed2f29bb9b8e30543395fdd86f69119c9c9e35
Jazper Garcia <jazpergarcia10@gmail.com>
2025-01-23 12:28:00 +0800

    Add WebSocket client and server implementation

---
+++ server/index.js
@@ -0,0 +1,28 @@
+// filepath: /d:/Documents/School Files/clarknav-websocket/server/index.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the client directory
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
