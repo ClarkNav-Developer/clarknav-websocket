bc9939af49ee4a7478a48d87c03cf95cfb85a016
Jazper Garcia <jazpergarcia10@gmail.com>
2025-01-23 13:51:07 +0800

    Refactor server setup by removing static file serving and updating package.json with project details

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// filepath: /d:/Documents/School Files/clarknav-websocket/server/index.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -7,13 +6,6 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
-// Serve static files from the client directory
-app.use(express.static(path.join(__dirname, '../client')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
